76d6503d0dafc8d896278cde51b71ea7f20c6f17
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-05-18 22:51:18 +0200

    add folder form

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log("hej wszystkim")
+{
+    const tasks = [
+        {
+            content: "zjeść pierogi",
+            done: false,
+        },
+        {
+            content: "umyć naczynia",
+            done: true,
+        },
+
+    ];
+
+    const render = () => {
+        let htmlString = "";
+        for (const taks of tasks) {
+            htmlString +=`
+            <li>
+                ${taks.content}
+            </li>
+            `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    }
+    init();
+}
